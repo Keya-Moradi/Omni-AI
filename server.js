5fefd439867857edc5cee76f5069c9e5848183fc
keya-moradi <keya.moradi@gmail.com>
2025-12-02 00:01:28 -0800

    Fix API endpoints, routing, and file structure

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static('public'));
 app.use(methodOverride('_method'));
 app.set('view engine', 'ejs');
